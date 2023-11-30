1b31b0efc569204d8a1081ff819ac9c329cc225c
AMAN KUMAR <92772172+AmanKumar2626@users.noreply.github.com>
2023-11-30 23:39:55 +0530

    Update mailapi.js

--- client/src/service/mailapi.js
+++ client/src/service/mailapi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { getEmail } from './mails';
-const API_BASE_URL = 'http://localhost:8000'; 
+const API_BASE_URL = 'https://easy-share-backend.onrender.com'; 
 
 export const sendEmailAPI = async () => {
   try {
